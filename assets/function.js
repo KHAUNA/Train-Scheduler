fb04928d5203f98493347fc2012df1a2801a9b44
KHAUNA <jtwinturbo@gmail.com>
2019-01-11 22:08:33 -0500

    final upload

--- assets/function.js
+++ assets/function.js
@@ -11,34 +11,80 @@
   firebase.initializeApp(config);
 
 var database = firebase.database();
-var trainName = "";
-var destination = "";
-var firstTrain = "";
-var frequency = "";
+// var trainName = "";
+// var destination = "";
+// var firstTrain = "";
+// var frequency = "";
+// var timeRemaining = "";
+// var diffTime = "";
+// var firstTrainConv = "";
+
 
 
 $("#submit").on("click", function(){
   event.preventDefault();
-  trainName = $("#trainName").val();
-  destination = $("#destination").val();
-  firstTrain = moment($("#first-train").val(), "HHmm")
-  frequency = $("#frequency").val();
+  var trainName = $("#trainName").val();
+  var destination = $("#destination").val();
+  var firstTrain = moment($("#first-train").val(), "HHmm")
+  var frequency = $("#frequency").val();
+  var currTime = moment().format("HH:mm");
 
-  firstTrain = firstTrain.format("HH:mm")
+   firstTrain = firstTrain.format("HH:mm")
+  var firstTrainConv = moment(firstTrain, "HH:mm").subtract(1, "years")
+  var diffTime = moment().diff(moment(firstTrainConv), "minutes");
+  var timeRemaining = (frequency - (diffTime % frequency))
+
 
   //10am train 1, and they come every 10 minutes
   // you get there at 10:12                                          
 
   // moment.HTML5_FMT.TIME
 
-  newRow = $("<tr>").append(
-    $("<td>").text(trainName),
-    $("<td>").text(destination),
-    $("<td>").text(frequency),
-    $("<td>").text(firstTrain)
-  )
-    $("#train-table").append(newRow)
+  // newRow = $("<tr>").append(
+  //   $("<td>").text(trainName),
+  //   $("<td>").text(destination),
+  //   $("<td>").text(frequency),
+  //   $("<td>").text(firstTrain),
+  //   $("<td>").text(timeRemaining)
+  // );
+  //   $("#train-table").append(newRow);
+
+  var addedTrain = {
+    train: trainName,
+    trainTo: destination,
+    trainFrom: firstTrain,
+    freq: frequency
+  };
+
+  database.ref().push(addedTrain);
+
+  $("#trainName").val("");
+  $("#destination").val("");
+  $("#first-train").val("");
+  $("#frequency").val("");
 
 })
-//use for relative time
-//moment().endOf('day').fromNow();          // in 3 hours
+    database.ref().on("child_added", function(childSnapshot) {
+      console.log(childSnapshot.val());
+
+
+      var trainName = childSnapshot.val().train;
+      var destination = childSnapshot.val().trainTo;
+      var firstTrain = childSnapshot.val().trainFrom;
+      var frequency = childSnapshot.val().freq;
+      var currTime = moment().format("HH:mm");
+
+      var firstTrainConv = moment(firstTrain, "HH:mm").subtract(1, "years")
+      var diffTime = moment().diff(moment(firstTrainConv), "minutes");
+      var timeRemaining = (frequency - (diffTime % frequency))
+
+      newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+        $("<td>").text(firstTrain),
+        $("<td>").text(timeRemaining)
+      );
+        $("#train-table").append(newRow);
+
+    });
