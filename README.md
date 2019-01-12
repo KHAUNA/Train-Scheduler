51ef9454753fb3548ff7081a617dadd518278cf1
KHAUNA <jtwinturbo@gmail.com>
2019-01-11 22:16:29 -0500

    actual final...cuz you always forget something

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Train-Scheduler
+
+Purpose
+
+This app allows you to enter a new train and it will populate that on the page.  IT will also calculate when that train will arrive based on the current time, starting train time, and the frequency that the train appears. 
--- assets/function.js
+++ assets/function.js
@@ -11,73 +11,56 @@
   firebase.initializeApp(config);
 
 var database = firebase.database();
-// var trainName = "";
-// var destination = "";
-// var firstTrain = "";
-// var frequency = "";
-// var timeRemaining = "";
-// var diffTime = "";
-// var firstTrainConv = "";
 
-
-
+//submit button on click
 $("#submit").on("click", function(){
   event.preventDefault();
+//capture entered values and store into variables
   var trainName = $("#trainName").val();
   var destination = $("#destination").val();
   var firstTrain = moment($("#first-train").val(), "HHmm")
   var frequency = $("#frequency").val();
   var currTime = moment().format("HH:mm");
+//math to determine when next train is
 
    firstTrain = firstTrain.format("HH:mm")
+//subtract 1 year to ensure first train time comes before the current time
+//math to find the remaining time to next train from current time
+//for math to work, current time must be after first train time
   var firstTrainConv = moment(firstTrain, "HH:mm").subtract(1, "years")
   var diffTime = moment().diff(moment(firstTrainConv), "minutes");
   var timeRemaining = (frequency - (diffTime % frequency))
-
-
-  //10am train 1, and they come every 10 minutes
-  // you get there at 10:12                                          
-
-  // moment.HTML5_FMT.TIME
-
-  // newRow = $("<tr>").append(
-  //   $("<td>").text(trainName),
-  //   $("<td>").text(destination),
-  //   $("<td>").text(frequency),
-  //   $("<td>").text(firstTrain),
-  //   $("<td>").text(timeRemaining)
-  // );
-  //   $("#train-table").append(newRow);
-
+//store variables in object
   var addedTrain = {
     train: trainName,
     trainTo: destination,
     trainFrom: firstTrain,
     freq: frequency
   };
-
+//push to firebase database
   database.ref().push(addedTrain);
-
+//clear out input fields
   $("#trainName").val("");
   $("#destination").val("");
   $("#first-train").val("");
   $("#frequency").val("");
 
 })
+//when a child is added to firebase, run following code
     database.ref().on("child_added", function(childSnapshot) {
       console.log(childSnapshot.val());
 
-
+//capture stored values and set to variables
       var trainName = childSnapshot.val().train;
       var destination = childSnapshot.val().trainTo;
       var firstTrain = childSnapshot.val().trainFrom;
       var frequency = childSnapshot.val().freq;
       var currTime = moment().format("HH:mm");
-
+//same math as above to calculate remaining time for next train
       var firstTrainConv = moment(firstTrain, "HH:mm").subtract(1, "years")
       var diffTime = moment().diff(moment(firstTrainConv), "minutes");
       var timeRemaining = (frequency - (diffTime % frequency))
-
+//put the stored database elements into the html display
       newRow = $("<tr>").append(
         $("<td>").text(trainName),
         $("<td>").text(destination),
